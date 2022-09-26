37345973d675b60f0f492783f03c7898a831593b
SarahAbera <114018578+SarahAbera@users.noreply.github.com>
2022-09-26 22:50:23 +0300

    Update How many numbers are smaller than the current number.py

--- How many numbers are smaller than the current number.py
+++ How many numbers are smaller than the current number.py
@@ -3,6 +3,6 @@ class Solution(object):
         count = [0]*len(nums)
         for i in range(len(nums)):
             for j in range(len(nums)):
-                if nums[j] < nums[i]:
-                    count[i] +=1
+                if nums[i] > nums[j]:
+                    count[i] += 1
         return count
