27034dd827773c4f959c131c58e2ef3fab6931d2
SarahAbera <114018578+SarahAbera@users.noreply.github.com>
2022-10-13 00:01:38 +0300

    Create Fruits into Baskets.py

---
+++ Fruits into Baskets.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        l, nums, res = 0, collections.Counter(), 0
+        for r in range(len(fruits)):
+            nums[fruits[r]] += 1
+            while len(nums) > 2:
+                nums[fruits[l]] -= 1 
+                if not nums[fruits[l]]:
+                    nums.pop(fruits[l])
+                l += 1
+            res = max(res, r - l + 1)
+        return res
